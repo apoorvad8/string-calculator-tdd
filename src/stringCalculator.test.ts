7098d5f8b86f72df4456bb0a65c23ce6f21b9712
apoorvad8 <apoorvadas264@gmail.com>
2025-02-11 09:47:08 +0530

    Add method for sum of 2 numbers input

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -5,9 +5,14 @@ describe('StringCalculator', () => {
     const calculator = new StringCalculator();
     expect(calculator.add('')).toBe(0);
   });
-  
+
   it('should return the number itself if only one number is in the string', () => {
     const calculator = new StringCalculator();
     expect(calculator.add('1')).toBe(1);
+  });
+
+  it('should return the sum of two numbers separated by a comma', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('1,2')).toBe(3);
   });
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,8 +1,12 @@
 export class StringCalculator {
-	add(numbers: string): number {
-		if (!numbers) {
-			return 0;
-		}
-		return parseInt(numbers, 10);
-	}
+  add(numbers: string): number {
+    if (!numbers) {
+      return 0;
+    }
+    const nums = numbers.split(',');
+    if (nums.length === 1) {
+      return parseInt(numbers, 10);
+    }
+    return parseInt(nums[0], 10) + parseInt(nums[1], 10);
+  }
 }
