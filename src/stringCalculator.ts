6cbda4a30c0958af3db6019db0b2f8437f5add66
apoorvad8 <apoorvadas264@gmail.com>
2025-02-11 09:38:02 +0530

    Add method for empty string

---
+++ src/stringCalculator.ts
@@ -0,0 +1,9 @@
+export class StringCalculator {
+    add(numbers: string): number {
+      if (numbers === '') {
+        return 0;
+      }
+      // more logic to be added
+      return 0;
+    }
+  }
