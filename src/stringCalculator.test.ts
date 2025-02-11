2384d90a3e94b0a845adc63e8017e3d6cf1c7835
apoorvad8 <apoorvadas264@gmail.com>
2025-02-11 10:13:16 +0530

    Add method with support for custom delimiters

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -26,4 +26,9 @@ describe('StringCalculator', () => {
     expect(calculator.add('1\n2,3')).toBe(6);
   });
 
+  it('should support different delimiters', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add('//;\n1;2')).toBe(3);
+  });
+
 });
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,13 +1,23 @@
 export class StringCalculator {
-  add(numbers: string): number {
-    if (!numbers) {
-      return 0;
-    }
-    const nums = numbers.split(/[,\n]/);
+	add(numbers: string): number {
+		if (!numbers) {
+			return 0;
+		}
+
+		let delimiter = /[,\n]/;
+		let numsStr = numbers;
+
+		if (numbers.startsWith("//")) {
+			const delimiterEnd = numbers.indexOf("\n");
+			delimiter = new RegExp(numbers.substring(2, delimiterEnd));
+			numsStr = numbers.substring(delimiterEnd + 1);
+		}
+
+		const nums = numsStr.split(delimiter);
 		let sum = 0;
 		for (const num of nums) {
 			sum += parseInt(num, 10);
 		}
 		return sum;
-  }
+	}
 }
